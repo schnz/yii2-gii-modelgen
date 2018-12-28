5bd295eb420329536d940d8b60cea783a2e0a432
Markus Schanz <coksnuss@googlemail.com>
2018-12-29 00:05:54 +0100

    Reverted previous change in gii.js that breaks expected behavior

--- assets/gii.js
+++ assets/gii.js
@@ -2,12 +2,14 @@
     const _init = yii.gii.init;
 
     yii.gii.init = function () {
-        _init.apply(this, arguments);
-
         // hide column name text fields when timstamp behavior option is disabled
         $('form #generator-includetimestampbehavior').change(function () {
             $('form .field-generator-createdcolumnname').toggle($(this).is(':checked'));
             $('form .field-generator-updatedcolumnname').toggle($(this).is(':checked'));
         }).change();
+
+        // Has to be called here because the change() call above would
+        // otherwise make generated code file section vanish.
+        _init.apply(this, arguments);
     }
 })(jQuery);
